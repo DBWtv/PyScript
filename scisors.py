9947f26446f5b89a1be64709155591b9df507e1b
dbw <dima.bwc@gmail.com>
2022-11-23 23:54:21 +0600

    change scisors a bit

--- scisors.py
+++ scisors.py
@@ -1,13 +1,15 @@
 import random
 input = Element("user_obj")
 op1 = Element("scisors_op")
-op2 = Element("scisors_test")
 
 kard = ['Rock', 'Paper', 'Scisors']
 
 def play(*args, **kwargs):
     i = random.randint(0,2)
-    if kard[i] == input.value:
-        op1.write(f'you lose, computer get:  {kard[i]}')
+    if input.value in kard:
+        if kard[i] == input.value:
+            op1.write(f'you lose, computer get:  {kard[i]}')
+        else:
+            op1.write(f'you win, computer get:  {kard[i]}')
     else:
-        op1.write(f'you win, computer get:  {kard[i]}')
+        op1.write(f'Input right object: Rock / Scisors / Paper')
