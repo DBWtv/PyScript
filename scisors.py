b2014af17a73ceebb1ee7c78757edb23841109cd
Dmitry <dima.bwc@gmial.com>
2022-11-24 10:04:23 +0600

    tryin more fitures

--- scisors.py
+++ scisors.py
@@ -1,6 +1,12 @@
 import random
 input = Element("user_obj")
 op1 = Element("scisors_op")
+op2 = Element("scisors_low")
+i_my_score = 0
+i_pc_score = 0
+
+op_my_score = Element("my_score")
+op_pc_score = Element("pc_score")
 
 kard = ['Rock', 'Paper', 'Scisors']
 
@@ -8,8 +14,15 @@ def play(*args, **kwargs):
     i = random.randint(0,2)
     if input.value in kard:
         if kard[i] == input.value:
-            op1.write(f'you lose, computer get:  {kard[i]}')
+            i_pc_score = i_pc_score + 1
+            op1.write(f'Computer choce:  {kard[i]}')
+            op2.write('You\'r lose the ')
         else:
-            op1.write(f'you win, computer get:  {kard[i]}')
+            i_my_score = i_my_score + 1
+            op1.write(f'Computer choce:  {kard[i]}')
+            op2.write('You\'r win the ')
     else:
         op1.write(f'Input right object: Rock / Scisors / Paper')
+
+op_my_score.write(i_my_score)
+op_pc_score.write(i_pc_score)
